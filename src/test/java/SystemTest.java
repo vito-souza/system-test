52486166d57503a3ce7d091e2c5fd617cbec8ca8
Vítor Souza <vitorlegalvh@gmail.com>
2024-12-12 12:39:45 -0300

    teste variáveis de ambiente

--- src/test/java/SystemTest.java
+++ src/test/java/SystemTest.java
@@ -1,11 +1,19 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 
 class SystemTest {
 
 	@Test
-	void shoudlReturnEnvVariable() {
+	void shouldReturnEnvVariable() {
 		assertEquals("vitorlegalvh@gmail.com", System.getenv("GMAIL_USERNAME"));
+	}
+
+	@Test
+	void shouldReturnEnvVariables() {
+		Map<String, String> env = System.getenv();
+		assertEquals(env, System.getenv());
 	}
 }
