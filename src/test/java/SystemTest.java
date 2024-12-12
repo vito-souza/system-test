96e63aa81ee12320819369445e02933e38e19d57
Vítor Souza <vitorlegalvh@gmail.com>
2024-12-12 12:58:40 -0300

    arraycopy

--- src/test/java/SystemTest.java
+++ src/test/java/SystemTest.java
@@ -1,3 +1,4 @@
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Map;
@@ -5,6 +6,17 @@ import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 class SystemTest {
+
+	@Test
+	void shouldCopySubArrayCorrectly() {
+		char[] array = { 'u', 't', 'o', 'p', 'i', 'a' };
+		char[] copy = new char[3];
+		char[] top = { 't', 'o', 'p' };
+
+		System.arraycopy(array, 1, copy, 0, 3);
+
+		assertArrayEquals(copy, top);
+	}
 
 	@Test
 	void shoudIdentifyObjectHashCode() {
